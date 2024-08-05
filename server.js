82a721e7f1902cb1b98777b4f7e831df488f970a
zdivyank <divyankzaveri10@gmail.com>
2024-08-05 23:35:42 +0530

    test

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ const userRoute = require('./routers/user_routers')
 const otpRoute = require('./routers/otp_routers')
 const path = require('path');
 
-app.use(cors({
-  origin: ['http://localhost:5173','https://frozenwonder-frontend.onrender.com','https://frozenwonders.netlify.app/']
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173','https://frozenwonder-frontend.onrender.com','https://frozenwonders.netlify.app/']
+// }));
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://frozenwonders.netlify.app/"); // Update this to your frontend URL
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use(express.json());
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
