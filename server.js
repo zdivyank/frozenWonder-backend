b446e6539cef6a1d2f4041bda7f57bda36b56532
zdivyank <divyankzaveri10@gmail.com>
2024-08-05 23:38:28 +0530

    updated cors

--- server.js
+++ server.js
@@ -15,13 +15,6 @@ const path = require('path');
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://frozenwonders.netlify.app/"); // Update this to your frontend URL
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
 app.use(express.json());
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
